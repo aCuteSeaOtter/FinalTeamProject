c4237b8afecc75cdaac4e340411bda8031a40ca7
aCuteSeaOtter <sedr8456@naver.com>
2024-07-15 12:16:35 +0900

    dsafas

---
+++ finalProject/src/main/resources/static/assets/js/contact-form.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function () {
+	jQuery('#submit').on('click',document,function(){
+			if(jQuery('#captcha_val').val()!=jQuery('#captcha_text').val()){
+				$('#captcha_text').parent('div').append('<span class="error">Captch is not match</span>');
+			}
+			else{
+				jQuery("#contactpage").validate({
+					submitHandler : function (e) {
+						submitSignupFormNow(jQuery("#contactpage"))
+					},
+					rules : {
+						fname : {
+							required : true
+						},
+						email : {
+							required : true,
+							email : true
+						}
+					},
+					errorElement : "span",
+					errorPlacement : function (e, t) {
+						e.appendTo(t.parent())
+					}
+				});
+				submitSignupFormNow = function (e) {
+					var t = e.serialize();
+					var n = "contact-form.php";
+					jQuery.ajax({
+						url : n,
+						type : "POST",
+						data : t,
+						success : function (e) {
+							var t = jQuery.parseJSON(e);
+							if (t.status == "Success") {
+								jQuery("#form_result").html('<span class="form-success alert alert-success d-block">' + t.msg + "</span>");
+							} else {
+								jQuery("#form_result").html('<span class="form-error alert alert-danger d-block">' + t.msg + "</span>")
+							}
+							jQuery("#form_result").show();
+						}
+					});
+					return false
+				}
+		}
+	});
+	
+})
---
+++ finalProject/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>새글등록</title>
+</head>
+<body>
+
+</body>
+</html>
